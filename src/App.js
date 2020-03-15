b93aa3361d612735f55ca15daa333391834275a9
Tam Nguyen <Tam_Nguyen@McKinsey.com>
2020-03-15 13:24:38 +0800

    Reformat and add instruction pane

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function _expandDateRange(startDate, endDate) {
     let newDate = new Date(startDate.valueOf());
     newDate.setDate(newDate.getDate() + i);
     newDate.setFullYear(year);
-      foundDates.push(_getDateString(newDate));
+    foundDates.push(_getDateString(newDate));
   }
   return foundDates;
 }
@@ -44,7 +44,7 @@ function parseDateLine(dateLine) {
   try {
     return [_getDateString(new Date(dateLine))];
   } catch (e) {
-    return []
+    return [];
   }
 }
 
@@ -70,7 +70,6 @@ function parseDatesByUsers(rawInput) {
 }
 
 function parseUsersByDates(dates_by_user) {
-
   let user_by_dates = {};
   for (let user of Object.keys(dates_by_user)) {
     for (let dt of dates_by_user[user]) {
@@ -81,19 +80,19 @@ function parseUsersByDates(dates_by_user) {
 }
 
 function chooseUsersForDates(usersByDates) {
-  let userPriority = {}
-  let chosenUsers = {}
+  let userPriority = {};
+  let chosenUsers = {};
   let definedDates = Object.keys(usersByDates).sort();
   let minDate = definedDates[0];
   let maxDate = definedDates[definedDates.length - 1];
   let allDates = _expandDateRange(minDate, maxDate);
-  for(let dt of allDates) {
+  for (let dt of allDates) {
     chosenUsers[dt] = null;
   }
-  for(let dt of definedDates) {
+  for (let dt of definedDates) {
     let dateUsers = usersByDates[dt];
     let minUser = null;
-    for(let user of dateUsers) {
+    for (let user of dateUsers) {
       if (minUser === null || userPriority[user] < userPriority[minUser]) {
         minUser = user;
       }
@@ -106,10 +105,8 @@ function chooseUsersForDates(usersByDates) {
 
 function formatChosenUsers(chosenUsers) {
   let outputLines = [];
-  for(let dt of Object.keys(chosenUsers).sort()) {
-    outputLines.push(
-      dt + ": " + (chosenUsers[dt] || "UNSPECIFIED")
-    )
+  for (let dt of Object.keys(chosenUsers).sort()) {
+    outputLines.push(dt + ": " + (chosenUsers[dt] || "UNSPECIFIED"));
   }
   return outputLines.join("\n");
 }
@@ -117,12 +114,14 @@ function formatChosenUsers(chosenUsers) {
 function App() {
   const [scheduleInput, setScheduleInput] = useState(`\
 # Xinli
+2020-01-01
+2020-01-03
 01-08 to 01-10
 01-10 to 01-15
 # Ahmad
 01-25 to 01-31
 # Frank
-01-01 to 01-05
+01-02 to 01-05
 01-10 to 01-15
 # Karen
 01-15 to 01-20
@@ -130,60 +129,97 @@ function App() {
 01-20 to 01-25
 # Tam
 2020-01-06
-2020-01-07`);
+2020-01-07
+2020-02-01`);
 
   let datesByUser = parseDatesByUsers(scheduleInput);
   let usersByDates = parseUsersByDates(datesByUser);
   let chosenUsers = chooseUsersForDates(usersByDates);
   let output = formatChosenUsers(chosenUsers);
 
+  const InputEditor = (
+    <AceEditor
+      placeholder="Placeholder Text"
+      mode="markdown"
+      theme="terminal"
+      name="blah2"
+      fontSize={14}
+      showPrintMargin={true}
+      showGutter={true}
+      highlightActiveLine={true}
+      value={scheduleInput}
+      setOptions={{
+        showLineNumbers: true,
+        tabSize: 2
+      }}
+      onChange={setScheduleInput}
+      height="40vh"
+    />
+  );
+  const OutputEditor = (
+    <AceEditor
+      mode="markdown"
+      theme="terminal"
+      name="blah2"
+      fontSize={14}
+      showPrintMargin={true}
+      showGutter={true}
+      highlightActiveLine={true}
+      value={output}
+      setOptions={{
+        showLineNumbers: true,
+        tabSize: 2
+      }}
+      height="40vh"
+      readOnly={true}
+    />
+  );
+
+  const InstructionEditor = (
+    <AceEditor
+      mode="markdown"
+      theme="terminal"
+      name="blah2"
+      fontSize={14}
+      showPrintMargin={true}
+      showGutter={false}
+      highlightActiveLine={true}
+      value={`\
+  Input the dates that you are available underneath "Available Dates".
+  Dates you are assigned to work will be available in the output.
+`}
+      setOptions={{
+        showLineNumbers: false,
+        tabSize: 2
+      }}
+      height="80vh"
+      readOnly={true}
+      wrapEnabled={true}
+    />
+  );
+
   return (
     <div className="App">
-      <div className="calendars">
       <h1>Co-op Childcare Scheduler</h1>
-        <div className="calendar-row">
-          <h2 className="calendar-label">Available Dates</h2>
-          <div className="calendar-editor">
-            <AceEditor
-              placeholder="Placeholder Text"
-              mode="markdown"
-              theme="terminal"
-              name="blah2"
-              fontSize={14}
-              showPrintMargin={true}
-              showGutter={true}
-              highlightActiveLine={true}
-              value={scheduleInput}
-              setOptions={{
-                showLineNumbers: true,
-                tabSize: 2
-              }}
-              onChange={setScheduleInput}
-              height="40vh"
-            />
+      <div className="calendars">
+        <div className="calendar-io">
+          <div className="calendar-row">
+            <div className="calendar-pair">
+              <h2 className="calendar-label">Available Dates</h2>
+              <div className="calendar-editor">{InputEditor}</div>
+            </div>
+          </div>
+          <div className="calendar-row">
+            <div className="calendar-pair">
+              <h2 className="calendar-label">Available Dates</h2>
+              <div className="calendar-editor">{OutputEditor}</div>
+            </div>
           </div>
         </div>
-        <div className="calendar-row">
-          <h2 className="calendar-label">Assigned Dates</h2>
-          <div className="calendar-editor">
-            <AceEditor
-              mode="markdown"
-              theme="terminal"
-              name="blah2"
-              fontSize={14}
-              showPrintMargin={true}
-              showGutter={true}
-              highlightActiveLine={true}
-              value={output}
-              setOptions={{
-                showLineNumbers: true,
-                tabSize: 2
-              }}
-              height="40vh"
-              readOnly={true}
-            />
-          </div>
-        </div>
+        {/* <div className="calendar-instruction">
+          <h2>Instructions</h2>
+            {InstructionEditor}
+        </div> */}
       </div>
     </div>
   );
